9985b54404f7611cc1cdfb940c9c556d388a5629
LCrespoG <lgarccre@myuax.com>
2024-12-18 13:06:11 +0100

    1º commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <locale>
+#include <iostream>
+
+#include "ejercicio1/Ejercicio1.h"
+#include "ejercicio2/Ejercicio2.h"
+#include "ejercicio3/Ejercicio3.h"
+#include "ejercicio4/Ejercicio4.h"
+#include "ejercicio5/Ejercicio5.h"
+#include "ejercicio6/Ejercicio6.h"
+
+int main() {
+
+    std::locale::global(std::locale(""));
+
+    int opt = 0;
+
+    std::cout << "Elija el ejercicio a ejecutar: ";
+    std::cin >> opt;
+
+    switch (opt) {
+    case 1: Ejercicio1(); break;
+    case 2: Ejercicio2(); break;
+    case 3: Ejercicio3(); break;
+    case 4: Ejercicio4(); break;
+    case 5: Ejercicio5(); break;
+    case 6: Ejercicio6(); break;
+
+
+    default:
+        std::cout << "Ejercicio no encontrado" << std::endl;
+    }
+
+    return 0;
+}
